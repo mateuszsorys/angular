c4a094e83c0898953037e19dca11ed9e8d07665b
Mateusz <m.sorys@i4b.pl>
2019-05-16 12:49:18 +0200

    Misja 1

--- space-app/src/app/app.module.ts
+++ space-app/src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { A2sCommModule } from 'a2s-comm';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
@@ -10,7 +11,8 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    A2sCommModule
   ],
   providers: [],
   bootstrap: [AppComponent]
