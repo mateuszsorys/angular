07e38754378bd5c6aae1510e52d79d6bc19fd060
Mateusz <m.sorys@i4b.pl>
2019-05-23 10:02:33 +0200

    Misja 4

--- space-app/src/app/space/hangar/hangar.component.ts
+++ space-app/src/app/space/hangar/hangar.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { BomberShip, FighterShip, SpaceShip } from '../space-ship';
+import { Pilot } from '../pilot';
 
 @Component({
   selector: 'app-hangar',
@@ -11,7 +12,7 @@ export class HangarComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
-    this.spaceShips.push(new FighterShip());
+    this.spaceShips.push(new FighterShip(new Pilot('Lee Adama', '/assets/adama.png')));
     this.spaceShips.push(new BomberShip());
   }
 
